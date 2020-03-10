16db306d08134aa7b3346791c845f4c489a4b404
Cinnamo0n <nolepagain@gmail.com>
2020-03-10 12:08:41 +0700

    calc2 pertemuan 2

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,23 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b= 6;
+		Console.Writeline("Hasil penambahan : {0} + {1} = {2}",a,b,penambahan (a,b));
+		Console.Writeline("Hasil pengurangan : {0} - {1} = {2}",a,b,pengurangan (a,b));
+		
+		Console.Writeline("\nTekan sembarang untuk keluar ");
+		Console.Readkey();
+	}
 	
+	static int penambahan(int a,int b)
+	{
+		return a + b;
+	}
+	
+	static int pengurangan(int a, int b)
+	{
+		return a-b;
 	}
 
 }
